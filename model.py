046c60e2c17baa6f8cda11ddb9d785581030dcc5
Shiling Maxim <max290606@gmail.com>
2025-02-11 21:01:56 +0300

    Сделан полноценный файл с моделью model.py

--- model.py
+++ model.py
@@ -1,20 +1,19 @@
-# !pip install transformers sentencepiece --quiet
 import torch
 import time
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup
 
 
 model_checkpoint = 'cointegrated/rubert-tiny-sentiment-balanced'
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
-if torch.cuda.is_available():
-    model.cuda()
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint) # токенизация
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint) # загрузка модели
 
 
-def get_sentiment(text, return_type='label'):
-    """ Calculate sentiment of a text. `return_type` can be 'label', 'score' or 'proba' """
+def get_sentiment(text: str, return_type: str = 'label') -> str | float | np.ndarray:
+    """Определяет тональность текста text. return_type может быть: 'label' - метка, 'score' - числовая оценка,
+    'proba' - вероятности для каждой метки"""
     with torch.no_grad():
         inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(model.device)
         proba = torch.sigmoid(model(**inputs).logits).cpu().numpy()[0]
@@ -24,7 +23,9 @@ def get_sentiment(text, return_type='label'):
         return proba.dot([-1, 0, 1])
     return proba
 
-def clean_html(text):
+
+def clean_html(text: str) -> str:
+    """Очищает текст text от HTML-разметки"""
     if pd.isna(text):  # Проверка на NaN
         return text
     soup = BeautifulSoup(text, 'html.parser')
@@ -37,6 +38,6 @@ start_time = time.time()
 # dataset
 dataset = pd.read_excel('dataset_comments_500/dataset_comments.xlsx')
 dataset['MessageText'] = dataset['MessageText'].apply(clean_html)
-sentiments = [get_sentiment(text) for text in dataset['MessageText']]
+sentiments = [get_sentiment(text) for text in dataset['MessageText']] # Результат
 
 print(time.time()-start_time)
